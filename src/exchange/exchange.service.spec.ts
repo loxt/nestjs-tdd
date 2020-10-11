cc8d89b14b916eb84a637ae440e0b703caf478a0
loxt <emannuelloxt@hotmail.com>
2020-10-11 15:06:02 -0400

    test: ensure convertAmount called getCurrent twice

--- src/exchange/exchange.service.spec.ts
+++ src/exchange/exchange.service.spec.ts
@@ -1,16 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ExchangeService } from './exchange.service';
+import { CurrenciesService, ExchangeService } from './exchange.service';
 import { BadRequestException } from '@nestjs/common';
 
 describe('ExchangeService', () => {
   let service: ExchangeService;
+  let currenciesService: CurrenciesService;
 
   beforeEach(async () => {
+    const currenciesServiceMock = {
+      getCurrency: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ExchangeService],
+      providers: [
+        ExchangeService,
+        {
+          provide: CurrenciesService,
+          useFactory: () => currenciesServiceMock,
+        },
+      ],
     }).compile();
 
     service = module.get<ExchangeService>(ExchangeService);
+    currenciesService = module.get<CurrenciesService>(CurrenciesService);
   });
 
   it('should be defined', () => {
@@ -36,6 +48,15 @@ describe('ExchangeService', () => {
           amount: 1,
         }),
       ).resolves.not.toThrow();
+    });
+
+    it('should be called getCurrency twice', async () => {
+      await service.convertAmount({
+        from: 'USD',
+        to: 'BRL',
+        amount: 1,
+      });
+      await expect(currenciesService.getCurrency).toBeCalledTimes(2);
     });
   });
 });
